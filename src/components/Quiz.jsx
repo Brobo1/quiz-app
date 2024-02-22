0a822b27c34f0afbec6b701d21cc8215dbbf2030
brobo1 <armanahz95@gmail.com>
2024-02-22 15:49:37 +0100

    Add summary statistics and user answers to Summary component
    The Summary component has been improved to show details of the quiz after completion. It now displays skipped, correct, and wrong answer percentages and lists user answers, indicating whether they're skipped, correct or wrong for better user feedback. It leverages the userAnswers prop passed to it from the Quiz component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import {useCallback, useState} from "react";
 import QUESTIONS               from "../questions.js";
-import quizCompleteImg         from "../assets/quiz-complete.png"
 import {Question}              from "./Question.jsx";
+import {Summary}               from "./Summary.jsx";
 
 export function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -23,10 +23,9 @@ export function Quiz() {
 	[handleSelectAnswer]);
   
   if (quizIsComplete) {
-	return <div id={"summary"}>
-	  <img src={quizCompleteImg} alt="quiz complete"/>
-	  <h2>Quiz Completed!</h2>
-	</div>
+	return (
+	  <Summary userAnswers={userAnswers}/>
+	);
   }
   
   return (
